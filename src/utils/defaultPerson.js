ee07e35a7ec3785545014a83040fb03f76e28f47
gmlunesa <gmlunesa@gmail.com>
2022-12-20 21:27:47 +0800

    🚮 Delete React files, fresh start

--- src/utils/defaultPerson.js
+++
@@ -1,7 +0,0 @@
-export const defaultPerson = {
-  name: "Odokawa",
-  city: "Tokyo, Japan",
-  hobbies: ["Driving", "Swimming", "Roadtripping"],
-  traits: ["Introverted", "Helpful", "Quiet"],
-  photo: "https://i.ibb.co/M9YLZ20/resize10.jpg",
-};
