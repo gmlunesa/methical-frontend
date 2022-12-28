69a7cd2aef0f1b64bf327860b32b6853248e8011
gmlunesa <gmlunesa@gmail.com>
2022-12-28 20:50:12 +0800

    Minor UI tweaks

--- components/Button.js
+++ components/Button.js
@@ -5,7 +5,7 @@ const Button = ({ handleClick, isLoading }) => {
     <button
       onClick={handleClick}
       disabled={isLoading}
-      className="rounded-lg text-center text-gray-700 py-2 px-4 bg-gray-200 border border-2 border-purple-500 p-0.5 shadow transition duration-200 hover:border-orange-400"
+      className="rounded-lg text-center text-gray-700 py-2 px-4 bg-gray-200 border-2 border-purple-500 p-0.5 shadow transition duration-200 hover:border-orange-400"
     >
       Generate profile
     </button>
